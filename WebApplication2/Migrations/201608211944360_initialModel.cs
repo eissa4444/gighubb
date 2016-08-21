f63ae512e12f78df48e99a91f558abd885210fbf
mohammed ebrahim fitooh eissa <eissa4444@yahoo.com>
2016-08-21 12:59:45 -0700

    enable migrations

--- WebApplication2/Migrations/201608211944360_initialModel.cs
+++ WebApplication2/Migrations/201608211944360_initialModel.cs
@@ -74,7 +74,7 @@ namespace WebApplication2.Migrations
                 .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
                 .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
                 .Index(t => t.UserId);
-            
+                
         }
         
         public override void Down()
